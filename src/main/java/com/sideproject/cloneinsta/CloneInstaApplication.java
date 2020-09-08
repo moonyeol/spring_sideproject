462b62ebc44d7ec6f3b606691a6b01cac0dd1235
moonyeol <ansduf05@gmail.com>
2020-09-08 12:19:01 +0900

    init project

---
+++ src/main/java/com/sideproject/cloneinsta/CloneInstaApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.cloneinsta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneInstaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneInstaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sideproject/cloneinsta/CloneInstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.cloneinsta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneInstaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
